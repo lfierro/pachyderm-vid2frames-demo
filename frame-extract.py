2465693c9b85699f42b00668f274a5f7cdfd7fb9
Lily <lfierro@github.com>
2020-11-03 21:40:20 -0500

    show error messaging when video file cannot be read

--- frame-extract.py
+++ frame-extract.py
@@ -5,7 +5,8 @@ import os
 
 def extract_frames(video):
     cap = cv2.VideoCapture(video)
-    folder_name = '/pfs/out/{}'.format(video.split('/')[-1].split('.')[0]) # create a folder with same name as video file
+    # create a folder with same name as video file
+    folder_name = '/pfs/out/{}'.format(video.split('/')[-1].split('.')[0])
     try:
         if not os.path.exists(folder_name):
             os.makedirs(folder_name)
@@ -16,15 +17,17 @@ def extract_frames(video):
     video_frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
     print('Total frames to be written: {}'.format(video_frame_count))
     success, frame = cap.read()
+    if not success:
+        print('Video at {} could not be read. Please check file format.'.format(video))
     while success:
-    # Capture frame-by-frame
+    # Capture each frame and save with numbered file name
         name = '{}/frame{}{}'.format(folder_name, str(currentFrame), '.jpg')
         print ('Creating...' + name)
         cv2.imwrite(name, frame)
         success, frame = cap.read()
         currentFrame += 1
 
-    # When everything done, release the capture
+    # When video stops reading, release capture and close windows
     cap.release()
     cv2.destroyAllWindows()
 
